52f77f48ee4a3ea98c2799cc9e7d1fb8cdb398d6
Vincent <vincent.vdt@proton.me>
2026-01-25 17:39:50 +0100

    style: update font settings and reset styles in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Scene } from "./components/common/Scene.tsx"
 const App = () => {
   return (
     <Scene>
-      <h1 className="text-4xl font-bold">Atelier</h1>
+      <h1 className="text-4xl font-bold font-display">Atelier</h1>
       {/* Write code here */}
     </Scene>
   )
