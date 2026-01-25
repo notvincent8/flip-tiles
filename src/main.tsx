17bb47c4704de55499a0b98eeceb721e20d0c69a
Vincent <vincent.vdt@proton.me>
2026-01-25 19:46:38 +0100

    feat: integrate GSAP library and register useGSAP plugin in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import { useGSAP } from "@gsap/react"
+
+import gsap from "gsap"
 import { ErrorBoundary } from "@/components/common/ErrorBoundary.tsx"
+
+gsap.registerPlugin(useGSAP)
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
