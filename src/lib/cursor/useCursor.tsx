e9b6f1ba2e8b5a7043dc158b0c88c69ef8d70b8d
Vincent <vincent.vdt@proton.me>
2026-01-25 19:46:49 +0100

    style: apply consistent formatting in useCursor hook

--- src/lib/cursor/useCursor.tsx
+++ src/lib/cursor/useCursor.tsx
@@ -1,23 +1,22 @@
-import {useEffect, useState} from "react";
-
+import { useEffect, useState } from "react"
 
 type MousePosition = {
   x: number
   y: number
 }
 const useCursor = () => {
-  const [position, setPosition] = useState<MousePosition>({x: 0, y: 0})
+  const [position, setPosition] = useState<MousePosition>({ x: 0, y: 0 })
 
   useEffect(() => {
     const updateMousePosition = (event: MouseEvent) => {
-      setPosition({x: event.clientX, y: event.clientY})
+      setPosition({ x: event.clientX, y: event.clientY })
     }
 
     globalThis.addEventListener("mousemove", updateMousePosition)
     return () => globalThis.removeEventListener("mousemove", updateMousePosition)
-  })
+  }, [])
 
-  return {position, getMousePosition: () => position}
-};
+  return { position, getMousePosition: () => position }
+}
 
-export default useCursor;
+export default useCursor
